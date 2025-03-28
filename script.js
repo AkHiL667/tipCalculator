b9405cb2170d70384d5ba66443cf3dad96e7479a
Akhil <konda.akhil456@gmail.com>
2025-03-28 09:54:40 +0530

    added local storage

--- script.js
+++ script.js
@@ -5,14 +5,37 @@ let percentageInput = document.querySelector('.jspercentage');
 let calcButton = document.querySelector('.jsbutton');
 let resetButton = document.querySelector('.jsreset')
 
-calcButton.addEventListener('click', () => {
+window.addEventListener('load', () => {
+  if (localStorage.getItem("bill")) {
+    billInput.value = localStorage.getItem("bill");
+  }
+  if (localStorage.getItem('percent')) {
+    percentageInput.value = localStorage.getItem('percent');
+  }
+  calculate();
+});
+
+
+function calculate() {
   let bill = parseFloat(billInput.value);
   let percent = parseFloat(percentageInput.value);
+  if (isNaN(bill) || isNaN(percent) || bill <= 0 || percent <= 0) {
+    tipDisplay.textContent = 0.00;
+    totalDisplay.textContent = 0.00;
+    return;
+  }
+
   let tip = bill * (percent / 100);
   let displayTip = tip.toFixed(2);
   tipDisplay.textContent = displayTip;
   let total = bill + tip;
   totalDisplay.textContent = total.toFixed(2);
+  localStorage.setItem('bill', bill);
+  localStorage.setItem('percent', percent);
+}
+
+calcButton.addEventListener('click', () => {
+  calculate();
 });
 
 resetButton.addEventListener('click', function () {
@@ -20,5 +43,7 @@ resetButton.addEventListener('click', function () {
   totalDisplay.textContent = 0;
   billInput.value = '';
   percentageInput.value = '';
+  localStorage.removeItem('bill');
+  localStorage.removeItem('percent');
 })
 
